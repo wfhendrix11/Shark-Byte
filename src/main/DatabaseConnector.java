6b878715bb721f208fa48a4970546cd61db78845
Unknown <jeremyroberts97@gmail.com>
2018-04-22 17:03:48 -0500

    Create controller, Connect controller, create DB class

---
+++ src/main/DatabaseConnector.java
@@ -0,0 +1,11 @@
+package main;
+import java.sql.*;
+
+public class DatabaseConnector {
+
+    //Attempt to connect to existing Database.
+    //If fails, creates the Database.
+    public DatabaseConnector(){
+
+    }
+}
