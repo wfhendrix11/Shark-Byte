62b6a06af386421e859b7508a0e7de21d4bc7a54
Azeoteric <las0061@auburn.edu>
2018-04-25 16:50:50 -0500

    added database test files

---
+++ src/databaseTest/DatabaseTest.java
@@ -0,0 +1,13 @@
+package databaseTest;
+
+import java.sql.Connection;
+import java.sql.Statement;
+
+public class DatabaseTest {
+    private String framework = "embedded";
+    private String protocol = "jdbc:derby:";
+
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+}
