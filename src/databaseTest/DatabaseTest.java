3d3b7a42e25eb0874e998f42fc04cd1e44a0142b
Azeoteric <las0061@auburn.edu>
2018-04-25 17:06:08 -0500

    added import statements to database

--- src/databaseTest/DatabaseTest.java
+++ src/databaseTest/DatabaseTest.java
@@ -1,11 +1,20 @@
 package databaseTest;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.Statement;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+
+import java.util.ArrayList;
+import java.util.Properties;
+
 
 public class DatabaseTest {
-    private String framework = "embedded";
-    private String protocol = "jdbc:derby:";
+    private final String framework = "embedded";
+    private final String protocol = "jdbc:derby:";
+    private final String databaseName = "sharkByteDB";
 
     public static void main(String[] args) {
         System.out.println("Test");
