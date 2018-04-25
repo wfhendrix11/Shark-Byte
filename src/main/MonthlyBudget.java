7930ec1a496e09bc37eff26de8627b2d5b954f4a
wfhendrix11 <wfhendrix11@gmail.com>
2018-04-25 17:04:25 -0500

    added MonthlyBudget and Category

--- src/main/MonthlyBudget.java
+++ src/main/MonthlyBudget.java
@@ -1,10 +1,31 @@
 package main;
 
-import java.util.ArrayList;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
 
 public class MonthlyBudget {
     private int month;
     private int year;
-    private ArrayList<Transaction> transactionList;
-    //private ArrayList<Category>
+    private Set<Transaction> transactions;
+    private Map<String, Category> categories;
+    private Set<String> labels;
+    private Map<String, Integer> budgets;
+
+    MonthlyBudget(int newMonth, int newYear) {
+        transactions = new TreeSet<Transaction>();
+        categories = new TreeMap<String, Category>();
+        labels = new TreeSet<String>();
+    }
+
+    public void addTransaction(Transaction t) {
+        transactions.add(t);
+        System.out.print("Transaction added.");
+    }
+
+    public void addCategory(String key, Category c) {
+        categories.put(key, c);
+        System.out.print("Category added.");
+    }
 }
